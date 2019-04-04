b255436157022d6dfb8c2f6a5a143cf4b5df4ffb
Nicolas Castro <nicolascastro@me.com>
2019-04-03 21:31:11 -0400

    Axios Post

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -107,4 +107,125 @@ const printTheChart = (stockData => {
   });
 });
 
+// When the WALL-E button is clicked
+document.getElementById("post-wall-e").onclick = function() {
+  // Create an object with data to submit
+  const characterInfo = {
+     name:       'WALL-E',
+     occupation: 'Waste Allocation Robot',
+     weapon:     'Head laser'
+   };
+   // Make a POST request
+   axios.post('https://ih-crud-api.herokuapp.com/characters',characterInfo)
+     .then(response => {
+          const { name, id } = response.data;
+          const newCharacterHtml = 
+             `
+               <li>
+                 <h3> ${name} </h3>
+                 <p> Id: ${id} </p>
+               </li>
+             `;
+         document.getElementById("characters-list").innerHTML += newCharacterHtml;
+         console.log('post successfull and the response is: ',response );
+     })
+     .catch(error => {
+         console.log('Oh No! Error is: ', error);  
+     })
+ }
+
+ document.getElementById("character-form").onsubmit = function(event) {
+  
+  event.preventDefault(); // <= !!!
+
+  console.log('form submit');
+};
+
+const theNames = document.getElementsByClassName("the-name");
+const theOccupations = document.getElementsByClassName("the-occupation");
+const theWeapons = document.getElementsByClassName("the-weapon");
+
+document.getElementById("character-form").onsubmit = function(event) {
+  event.preventDefault();
+ 
+  const characterInfo = {
+     name: theNames[0].value,
+     occupation: theOccupations[0].value,
+     weapon: theWeapons[0].value
+  };
+ 
+   axios.post('https://ih-crud-api.herokuapp.com/characters', characterInfo)
+     .then(response => {
+         const { name, id } = response.data;
+         const newCharacterHtml = `
+         <li>
+           <h3> ${name} </h3>
+           <p> Id: ${id} </p>
+         </li>
+         `;
+         document.getElementById("characters-list").innerHTML += newCharacterHtml;
+       // Clear the form after submitting:
+         document.getElementById("character-form").reset();
+
+     })
+     .catch(error => {
+         console.log("Error is: ", error);
+     })
+}
+
+document.getElementById("getButton").onclick = function(event){
+  const theId = document.getElementById("theCharId").value;
+  axios.get(`https://ih-crud-api.herokuapp.com/characters/${theId}`)
+      .then(response => {
+          console.log('Response from the API is: ', response);
+      })
+      .catch(error => {
+          console.log("The error is: ", error);
+      });
+}
+
+document.getElementById("getButton").onclick = function(event){
+  const theId = document.getElementById("theCharId").value;
+  axios.get(`https://ih-crud-api.herokuapp.com/characters/${theId}`)
+    .then(response => {
+        // console.log('Response from the API is: ', response.data);
+        
+        // The following lane hides the form to create a new character when we are updating one
+        document.getElementById("character-form").style.display = "none";
+        document.getElementById("updateForm").style.display = "block";
+        theNames[1].value = response.data.name;
+        theOccupations[1].value = response.data.occupation;
+        theWeapons[1].value = response.data.weapon;
+    })
+    .catch(error => {
+    // console.log("The error is: ", error);
+        document.getElementById("updateForm").style.display="none";
+        if(error.response.status === 404){
+            const errorMessage = `There's no character with id: ${theId}. Try some other ID.`
+            const errDiv = document.createElement("div");
+            errDiv.setAttribute("id", "error");
+            errDiv.innerHTML = errorMessage;
+            document.body.appendChild(errDiv);
+        }
+  });
+}
+document.getElementById("update-form").onsubmit = function(event){
+  event.preventDefault();
+  const theId = document.getElementById("theCharId").value;
+  const updatedcharacterInfo = {
+    name: theNames[1].value,
+    occupation: theOccupations[1].value,
+    weapon: theWeapons[1].value
+  };
+  
+  axios.patch(`https://ih-crud-api.herokuapp.com/characters/${theId}`, updatedcharacterInfo)
+    .then(response => {
+          console.log('update successful: ', response);
+          document.getElementById("update-form").reset();
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
 }, false);
